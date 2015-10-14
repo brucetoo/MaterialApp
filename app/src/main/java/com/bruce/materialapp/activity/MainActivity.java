8104d2355ac187820bfba4bc9c5528950421be80
brucetoo <brucetoo14@gmail.com>
2015-10-14 13:31:27 +0800

    试试..

--- app/src/main/java/com/bruce/materialapp/activity/MainActivity.java
+++ app/src/main/java/com/bruce/materialapp/activity/MainActivity.java
@@ -153,6 +153,7 @@ public class MainActivity extends BaseActivity implements FeedAdapter.OnFeedItem
                         startContentAnimation();
                     }
                 }).start();
+
     }
 
     /**
